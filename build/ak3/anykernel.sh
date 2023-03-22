0fec9a8225f93c4ff9e56380c117677ec7b93c37
enesuzun2002 <emurat.uzun@gmail.com>
2023-03-22 18:55:50 +0300

    ak3: fix .sh script perms

--- build/ak3/anykernel.sh
+++ build/ak3/anykernel.sh
@@ -30,7 +30,7 @@ is_slot_device=0;
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin $ramdisk/*.sh;
 
 
 ## AnyKernel boot install
