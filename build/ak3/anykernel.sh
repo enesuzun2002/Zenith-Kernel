19adf3ac64786293b5a10c1b84775702e65081f8
enesuzun2002 <emurat.uzun@gmail.com>
2023-03-24 11:43:35 +0300

    build: add modified secure storage blobs for wifi/bt

--- build/ak3/anykernel.sh
+++ build/ak3/anykernel.sh
@@ -6,8 +6,8 @@
 properties() { '
 kernel.string=Zenith-Kernel
 do.devicecheck=0
-do.modules=0
-do.systemless=0
+do.modules=1
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=zeroflte
