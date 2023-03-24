cac8550d433ad0402aeaf66fbb750b293e80268c
enesuzun2002 <emurat.uzun@gmail.com>
2023-03-24 13:11:05 +0300

    build: import irqbalance from aosp

--- build/ak3/anykernel.sh
+++ build/ak3/anykernel.sh
@@ -30,7 +30,7 @@ is_slot_device=0;
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin $ramdisk/*.sh;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin $ramdisk/*.sh $home/modules/system/bin/irqbalance;
 
 
 ## AnyKernel boot install
